0f2f984a0392ced46ece6acf6ea9ca4b9c096dad
Joshua Allen <joshutallen99@gmail.com>
2022-12-13 16:30:06 -0700

    new settings

--- studentnetwork/settings.py
+++ studentnetwork/settings.py
@@ -1,6 +1,8 @@
 import os
 from getpass import getpass
 from dotenv import load_dotenv
+import dj_database_url
+
 
 
 """
@@ -84,7 +86,13 @@ WSGI_APPLICATION = 'studentnetwork.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+DATABASE_URL = os.getenv("DATABASE_URL")
 
+DATABASES = {
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
+}
+
+'''
 DATABASES = {
     'default': {
     'ENGINE': 'django.db.backends.postgresql',
@@ -94,7 +102,7 @@ DATABASES = {
     'HOST': 'localhost'
     }
 }   
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
